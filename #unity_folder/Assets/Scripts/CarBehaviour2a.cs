103dd705a5447d718ee83274187256456d9cb874
João Silva <j0a0carll21@gmail.com>
2022-06-11 12:13:06 +0100

    Update #unity_folder

---
+++ #unity_folder/Assets/Scripts/CarBehaviour2a.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CarBehaviour2a : CarBehaviour {
+	
+	void LateUpdate()
+	{
+		// YOUR CODE HERE
+		float leftSensor = 0, rightSensor = 0;
+
+		//Read sensor values
+		if (DetectLights)
+        {
+			leftSensor = LeftLD.GetOutput();
+			rightSensor = RightLD.GetOutput();
+		}
+
+		if (DetectCars)
+        {
+			leftSensor = LeftCD.GetOutput();
+			rightSensor = RightCD.GetOutput(); 
+		}
+		
+
+		//Calculate target motor values
+		m_LeftWheelSpeed = leftSensor * MaxSpeed;
+		m_RightWheelSpeed = rightSensor * MaxSpeed;
+	}
+
+	private void OnTriggerEnter(Collider other){
+        // Se o player colidir com a pickup
+        if (other.gameObject.CompareTag("Coin")){
+            other.gameObject.SetActive(false); // desaparece
+        }
+    }
+}
