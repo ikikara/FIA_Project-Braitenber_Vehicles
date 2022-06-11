3e597ad23f29fbe021d90213bfcaec2a7bb5a852
João Silva <82664001+ikikara@users.noreply.github.com>
2022-06-11 13:08:31 +0100

    Update Rotate_Coin.cs

--- src/Rotate_Coin.cs
+++ src/Rotate_Coin.cs
@@ -14,10 +14,11 @@ public class Rotate_Coin : MonoBehaviour{
     void Update(){
         this.transform.Rotate(rotation * Time.deltaTime);
     }
-    
+    /*
     private void OnTriggerEnter(Collider other){
         if (other.name == "Searcher"){         // Se o player colidir com a pickup
             Destroy(gameObject);
         }
     }
+    */
 }
